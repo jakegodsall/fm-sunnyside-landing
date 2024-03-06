4f7290f49c9c8dec64a793965ea37b0e0e4be635
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-06 09:53:20 +0000

    added triangle at the top corner of mobile menu

--- src/app/components/MobileMenu/MobileMenu.jsx
+++ src/app/components/MobileMenu/MobileMenu.jsx
@@ -1,6 +1,6 @@
 export default function MobileMenu() {
     return (
-        <div className="fixed top-[8rem] z-30 flex w-[90%] flex-col items-center bg-white p-[4rem] text-center">
+        <div className="fixed top-[12rem] z-30 flex w-[90%] flex-col items-center border-b-white bg-white p-[4rem] text-center before:absolute before:right-0 before:top-[-2.4rem] before:h-[2.4rem] before:w-[2.4rem] before:border-b-[2.4rem] before:border-l-[2.4rem] before:border-white  before:border-l-transparent">
             <nav>
                 <ul className="mb-[4.8rem] flex flex-col gap-[3.2rem]">
                     <li className="font-sans text-[2rem] text-grey">About</li>
