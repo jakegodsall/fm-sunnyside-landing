6f4cc7809255f73417269887a3571185d777e385
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-09 09:22:04 +0100

    implemented desktop navigation bar

---
+++ src/app/components/DesktopNav/DesktopNav.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+export default function DesktopNav() {
+    return (
+        <nav>
+            <ul className="flex gap-[4.7rem]">
+                <li>
+                    <Link
+                        href="#"
+                        className="font-sans text-[1.8rem] text-white"
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="#"
+                        className="font-sans text-[1.8rem] text-white"
+                    >
+                        Services
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="#"
+                        className="font-sans text-[1.8rem] text-white"
+                    >
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="#"
+                        className="rounded-full bg-[#77cdf5] px-[3.2rem] py-[1.6rem] font-serif text-[1.8rem] uppercase text-white"
+                    >
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import { AnimatePresence } from "framer-motion";
 import logo from "@images/logo.svg";
 import HamburgerButton from "../UI/HamburgerButton/HamburgerButton";
 import MobileMenu from "../MobileMenu/MobileMenu";
+import DesktopNav from "../DesktopNav/DesktopNav";
 
 export default function Header() {
     const [hamburgerActive, setHamburgerActive] = useState(false);
@@ -19,10 +20,15 @@ export default function Header() {
         <>
             <header className="absolute z-20 flex w-full items-center justify-between px-[2rem] pb-[8.8rem] pt-[4rem]">
                 <Image src={logo} alt="logo" width={124} height={24} />
-                <HamburgerButton
-                    hamburgerActive={hamburgerActive}
-                    setHamburgerActive={handleHamburgerClicked}
-                />
+                <div className="sm:hidden">
+                    <HamburgerButton
+                        hamburgerActive={hamburgerActive}
+                        setHamburgerActive={handleHamburgerClicked}
+                    />
+                </div>
+                <div className="hidden sm:block">
+                    <DesktopNav />
+                </div>
             </header>
             <AnimatePresence>
                 {hamburgerActive && <MobileMenu />}
