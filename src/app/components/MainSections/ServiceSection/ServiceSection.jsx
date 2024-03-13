39767b6c260bed0a5029fbe7df4b395de6c7a482
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-13 14:20:49 +0100

    fixed the ServiceGrid in desktop viewport

--- src/app/components/MainSections/ServiceSection/ServiceSection.jsx
+++ src/app/components/MainSections/ServiceSection/ServiceSection.jsx
@@ -8,7 +8,7 @@ export default function ServiceSection({
     textColor,
 }) {
     return (
-        <section className="relative flex min-h-[60rem] w-screen flex-col items-center text-center">
+        <section className="relative flex min-h-[60rem] w-screen flex-col items-center text-center sm:w-1/2">
             <Image
                 src={image}
                 alt={alt}
@@ -16,12 +16,12 @@ export default function ServiceSection({
                 style={{ objectFit: "cover", objectPosition: "center" }}
             />
             <div
-                className={`absolute bottom-[2rem] px-[2rem] ${textColor} max-w-[34rem] sm:bottom-[-5rem] sm:h-[22rem]`}
+                className={`absolute bottom-[2rem] px-[2rem] ${textColor} max-w-[34rem] sm:bottom-[-2rem] sm:h-[22rem]`}
             >
                 <h2 className="mb-[2.7rem] font-serif text-[2.8rem]">
                     {title}
                 </h2>
-                <p className="mb-[4rem] font-sans text-[1.6rem] leading-[2.7rem] ">
+                <p className="mb-[4rem] font-sans text-[1.6rem] leading-[2.7rem] sm:mb-0">
                     {description}
                 </p>
             </div>
